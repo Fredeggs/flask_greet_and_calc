15053587c5b922ec6a652cc089c8ae2f841807bb
fredeggs <ian.dauphin@gmail.com>
2022-05-16 20:34:02 -0400

    completed basic assignment

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,31 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def addition():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(add(a,b))
+
+
+@app.route('/sub')
+def subtraction():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(sub(a,b))
+
+
+@app.route('/mult')
+def multiplication():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(mult(a,b))
+
+
+@app.route('/div')
+def divide():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(div(a,b))
